c1124a27974e5c201ab7c087015c65a0ae182847
Drew <dlazzeri1@gmail.com>
2020-07-15 15:26:59 -0600

    Move pubsub to main

--- index.js
+++ index.js
@@ -1,105 +1,106 @@
-import "babel-polyfill";
-import Libp2p from "libp2p";
-import Websockets from "libp2p-websockets";
-import WebRTCStar from "libp2p-webrtc-star";
-import { NOISE } from "libp2p-noise";
-import Secio from "libp2p-secio";
-import Mplex from "libp2p-mplex";
-import Boostrap from "libp2p-bootstrap";
-import { Elm } from "./src/Main.elm";
+"use strict";
 
-//My protocol
-const RayoProtocol = require("./src/chat");
+// Libp2p Core
+const Libp2p = require("libp2p");
+// Transports
+const Websockets = require("libp2p-websockets");
+const WebrtcStar = require("libp2p-webrtc-star");
+const wrtc = require("wrtc");
+// Stream Muxer
+const Mplex = require("libp2p-mplex");
+// Connection Encryption
+const { NOISE } = require("libp2p-noise");
+const Secio = require("libp2p-secio");
+// Chat over Pubsub
+const PubsubChat = require("./chat");
+// Peer Discovery
+const Bootstrap = require("libp2p-bootstrap");
+const KadDHT = require("libp2p-kad-dht");
+// PubSub implementation
 const Gossipsub = require("libp2p-gossipsub");
 
-Elm.Main.init({
-  node: document.querySelector("#elm"),
-});
-
-document.addEventListener("DOMContentLoaded", async () => {
-  // Create our libp2p node
+async function main() {
+  // Create the Node
   const libp2p = await Libp2p.create({
     addresses: {
-      // Add the signaling server address, along with our PeerId to our multiaddrs list
-      // libp2p will automatically attempt to dial to the signaling server so that it can
-      // receive inbound connections from other peers
-      listen: [
-        "/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star",
-        "/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star",
-      ],
+      listen: [`/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star`],
     },
     modules: {
-      transport: [Websockets, WebRTCStar],
-      connEncryption: [NOISE, Secio],
+      transport: [Websockets, WebrtcStar],
       streamMuxer: [Mplex],
-      peerDiscovery: [Boostrap],
+      connEncryption: [NOISE, Secio],
+      peerDiscovery: [Bootstrap],
+      dht: KadDHT,
       pubsub: Gossipsub,
     },
     config: {
+      transport: {
+        [WebrtcStar.prototype[Symbol.toStringTag]]: {
+          wrtc,
+        },
+      },
       peerDiscovery: {
         bootstrap: {
-          enabled: true,
           list: [
-            "/dns4/ams-1.bootstrap.libp2p.io/tcp/443/wss/p2p/QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd",
-            "/dns4/lon-1.bootstrap.libp2p.io/tcp/443/wss/p2p/QmSoLMeWqB7YGVLJN3pNLQpmmEk35v6wYtsMGLzSr5QBU3",
-            "/dns4/sfo-3.bootstrap.libp2p.io/tcp/443/wss/p2p/QmSoLPppuBtQSGwKDZT2M73ULpjvfd3aZ6ha4oFGL1KrGM",
-            "/dns4/sgp-1.bootstrap.libp2p.io/tcp/443/wss/p2p/QmSoLSafTMBsPKadTEgaXctDQVcqN88CNLHXMkTNwMKPnu",
-            "/dns4/nyc-1.bootstrap.libp2p.io/tcp/443/wss/p2p/QmSoLueR4xBeUbY9WZ9xGUUxunbKWcrNFTDAadQJmocnWm",
-            "/dns4/nyc-2.bootstrap.libp2p.io/tcp/443/wss/p2p/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64",
+            "/dnsaddr/sjc-1.bootstrap.libp2p.io/tcp/4001/ipfs/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
           ],
+        },
+      },
+      dht: {
+        enabled: true,
+        randomWalk: {
+          enabled: true,
         },
       },
     },
   });
 
-  // UI elements
-  const status = document.getElementById("status");
-  const output = document.getElementById("output");
-
-  output.textContent = "";
-
-  function log(txt) {
-    console.info(txt);
-    output.textContent += `${txt.trim()}\n`;
-  }
-
-  // Listen for new peers
-  libp2p.on("peer:discovery", (peerId) => {
-    log(`Found peer ${peerId.toB58String()}`);
-  });
-
-  // Listen for new connections to peers
+  // Listen on libp2p for `peer:connect` and log the provided connection.remotePeer.toB58String() peer id string.
   libp2p.connectionManager.on("peer:connect", (connection) => {
-    log(`Connected to ${connection.remotePeer.toB58String()}`);
+    console.info(`Connected to ${connection.remotePeer.toB58String()}!`);
   });
 
-  // Listen for peers disconnecting
-  libp2p.connectionManager.on("peer:disconnect", (connection) => {
-    log(`Disconnected from ${connection.remotePeer.toB58String()}`);
-  });
-
+  // Start libp2p
   await libp2p.start();
-  status.innerText = "libp2p started!";
-  log(`libp2p id is ${libp2p.peerId.toB58String()}`);
 
-  // Export libp2p to the window so you can play with the API
-  window.libp2p = libp2p;
-  var currentSeed = 10;
-  log(`the number has been updated to ${currentSeed}`);
+  // Log our PeerId and Multiaddrs
+  console.info(`${libp2p.peerId.toB58String()} listening on addresses:`);
+  console.info(
+    libp2p.multiaddrs.map((addr) => addr.toString()).join("\n"),
+    "\n"
+  );
 
-  var rayoProtocol = new RayoProtocol(
+  // Create our PubsubChat client
+  const pubsubChat = new PubsubChat(
     libp2p,
-    RayoProtocol.TOPIC,
-    ({ updatedSeed }) => {
-      currentSeed = updatedSeed;
-      log(`the number has been updated to ${updatedSeed}`);
+    PubsubChat.TOPIC,
+    ({ from, message }) => {
+      let fromMe = from === libp2p.peerId.toB58String();
+      let user = from.substring(0, 6);
+      if (pubsubChat.userHandles.has(from)) {
+        user = pubsubChat.userHandles.get(from);
+      }
+      console.info(
+        `${fromMe ? PubsubChat.CLEARLINE : ""}${user}(${new Date(
+          message.created
+        ).toLocaleTimeString()}): ${message.data}`
+      );
     }
   );
 
-  document
-    .getElementById("increase")
-    .addEventListener("click", async function () {
-      await rayoProtocol.send(5);
-    });
-  rayoProtocol.onStart();
-});
+  // Set up our input handler
+  setInterval(async () => {
+    // Remove trailing newline
+    // If there was a command, exit early
+    //if (pubsubChat.checkCommand("")) return;
+
+    try {
+      // Publish the message
+      await pubsubChat.send("Checking in");
+    } catch (err) {
+      console.error("Could not publish chat", err);
+    }
+  }, 1000);
+}
+
+main();
