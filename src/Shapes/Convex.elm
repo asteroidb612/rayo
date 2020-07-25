751ebbf5a75d36252646d051f1a997b0af581868
Drew <dlazzeri1@gmail.com>
2020-07-25 10:53:06 -0600

    Colliding icosahedrons

--- src/Shapes/Convex.elm
+++ src/Shapes/Convex.elm
@@ -127,8 +127,90 @@ computeNormal v1 v2 v3 =
     Vec3.normalize (Vec3.cross (Vec3.sub v3 v2) (Vec3.sub v1 v2))
 
 
-fromBlock : Float -> Float -> Float -> Convex
-fromBlock x y z =
+
+--icosahedron : Convex
+
+
+fromBlock _ _ _ =
+    let
+        -- Thanks http://rbwhitaker.wikidot.com/index-and-vertex-buffers !
+        v0 =
+            { x = -0.262865, y = 0.0, z = 0.425325 }
+
+        v1 =
+            { x = 0.262865, y = 0.0, z = 0.425325 }
+
+        v2 =
+            { x = -0.262865, y = 0.0, z = -0.425325 }
+
+        v3 =
+            { x = 0.262865, y = 0.0, z = -0.425325 }
+
+        v4 =
+            { x = 0.0, y = 0.425325, z = 0.262865 }
+
+        v5 =
+            { x = 0.0, y = 0.425325, z = -0.262865 }
+
+        v6 =
+            { x = 0.0, y = -0.425325, z = 0.262865 }
+
+        v7 =
+            { x = 0.0, y = -0.425325, z = -0.262865 }
+
+        v8 =
+            { x = 0.425325, y = 0.262865, z = 0.0 }
+
+        v9 =
+            { x = -0.425325, y = 0.262865, z = 0.0 }
+
+        v10 =
+            { x = 0.425325, y = -0.262865, z = 0.0 }
+
+        v11 =
+            { x = -0.425325, y = -0.262865, z = 0.0 }
+
+        faces =
+            [ ( v0, v6, v1 )
+            , ( v0, v11, v6 )
+            , ( v1, v4, v0 )
+            , ( v1, v8, v4 )
+            , ( v1, v10, v8 )
+            , ( v2, v5, v3 )
+            , ( v2, v9, v5 )
+            , ( v2, v11, v9 )
+            , ( v3, v7, v2 )
+            , ( v3, v10, v7 )
+            , ( v4, v8, v5 )
+            , ( v4, v9, v0 )
+            , ( v5, v8, v3 )
+            , ( v5, v9, v4 )
+            , ( v6, v10, v1 )
+            , ( v6, v11, v7 )
+            , ( v7, v10, v6 )
+            , ( v7, v11, v2 )
+            , ( v8, v10, v3 )
+            , ( v9, v11, v0 )
+            ]
+    in
+    { faces =
+        List.map
+            (\( x, y, z ) ->
+                { vertices = [ z, y, x ]
+                , normal = Vec3.cross (Vec3.sub y z) (Vec3.sub y x)
+                }
+            )
+            faces
+    , vertices = [ v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11 ]
+    , uniqueEdges = [] -- Maybe we don't need this? It's tough...
+    , uniqueNormals = List.map (\( x, y, z ) -> Vec3.cross (Vec3.sub y z) (Vec3.sub y x)) faces
+    , volume = 0 -- Maybe we don't need this?
+    , position = Vec3.zero
+    }
+
+
+fromunBlock : Float -> Float -> Float -> Convex
+fromunBlock x y z =
     let
         v0 =
             { x = -x, y = -y, z = -z }
